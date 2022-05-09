4e5888634921e576f1b629ff687e7d110cc383aa
45sjp <96190349+45sjp@users.noreply.github.com>
2022-05-09 13:35:55 +0900

    Cat#age 추가

--- hello-git/src/com/kh/animal/Cat.java
+++ hello-git/src/com/kh/animal/Cat.java
@@ -4,6 +4,7 @@ public class Cat {
 	private String name; // master에서 추가
 	private String color; // animal에서 추가
 	private String breed; // master에서 추가
+	private int age; // github에서 추가
 	
 	public void meow() {
 		System.out.println("야옹");
